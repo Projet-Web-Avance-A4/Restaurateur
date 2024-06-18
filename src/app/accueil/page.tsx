06227cb3bb6024a4796b76c01ddad46f7a407e10
Nicolas FOUQUE <nicolas.fouque@laposte.net>
2024-06-18 11:43:00 +0200

    update main to accueil

--- src/app/accueil/page.tsx
+++ src/app/accueil/page.tsx
@@ -53,14 +53,15 @@ export default function Home() {
   return (
     <NextUIProvider className="h-screen bg-beige">
       <Header
-        title="Livreur"
+        title="Restaurateur"
         showMyAccount={true}
         showStats={false}
       />
       <div className="container mx-auto">
         <div className="flex flex-wrap place-content-center">
-          <CustomCard title="Commande Disponible" description="Accéder à l'ensemble des commandes disponibles" href="commande_disponible" btnText="Accéder" />
-          <CustomCard title="Votre Commande en Cours" description="Accéder à votre commande Actuelle" href="suivi_commande" btnText="Accéder" />
+          <CustomCard title="Gestion de vos articles" href="article_page" btnText="Accéder" />
+          <CustomCard title="Gestion de vos menus" href="menu_page" btnText="Accéder" />
+          <CustomCard title="Suivi de vos commandes" href="suivi_commandes" btnText="Accéder" />
         </div>
       </div>
       <Footer />
